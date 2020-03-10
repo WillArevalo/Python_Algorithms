36544758066ab663a8d7733c20564850fcc33559
WillArevalo <willarevalo.developer@gmail.com>
2020-03-10 13:39:46 -0500

    Agrego Lineal_search

---
+++ Algoritmos_busqueda_y_ordenamiento/lineal_serch.py
@@ -0,0 +1,24 @@
+# _*_ coding: utf-8 _*_
+"""
+This module is for make a lineal search.
+ https://wiki.python.org/moin/TimeComplexity
+"""
+import random
+
+def lineal_search(lista, objetivo):
+    """Lineal Search."""
+    match = False
+    for el in lista:
+        if el == objetivo:
+            match = True
+            break
+    
+    return match
+
+if __name__ == "__main__":
+    len_of_list = int(input("What length of list want? "))
+    objetivo = int(input('What is the number for search? '))
+    lista = [random.randint(0,100) for i in range(len_of_list)]
+    print(*lista, sep=", ")
+    encontrado = lineal_search(lista, objetivo)
+    print(f'El elemento {objetivo} {"esta" if encontrado else "no esta"} en la lista.')
